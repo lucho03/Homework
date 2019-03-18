2afc4fe20b43c71485d22e79adc89a1168cf92b3
Lachezar Ivanov <37477952+lucho03@users.noreply.github.com>
2019-03-19 00:05:05 +0200

    Homework.

---
+++ Homework/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+using namespace std;
+double square_root (double n, int s);
+int main()
+{
+    printf("%f", square_root(69,11));
+    return 0;
+}
+
+double square_root(double n, int s)
+{
+    double p = n, x, u;
+    u = (p + n/p) / 2;
+    for (int i = 1; i < s; i++){
+        u = (u + n/u) / 2;
+    }
+    return u;
+}
